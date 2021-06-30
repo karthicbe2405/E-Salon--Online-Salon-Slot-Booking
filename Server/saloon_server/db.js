4f29094a7705c9d53626f5db7291657812c995c2
karthicbe2405 <karthicbe2405@gmail.com>
2021-06-30 14:51:35 +0530

    Used envireonment variable for db connection URL

--- Server/saloon_server/db.js
+++ Server/saloon_server/db.js
@@ -1,6 +1,7 @@
 const mongoose=require('mongoose')
-const mongo_URI = 'mongodb+srv://karthicbe2405:Tacmmongodb22@cluster0.p3b32.mongodb.net/esalon?retryWrites=true&w=majority';
-//const mongo_URI='mongodb+srv://saloon:harikrish@cluster0.8fdf2.mongodb.net/saloon?retryWrites=true&w=majority';
+const dotenv = require('dotenv')
+dotenv.config()
+const mongo_URI = process.env.dbConnectionURL;
 
 let db = mongoose.connect(mongo_URI,{useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify:false},(err)=>{
     if(!err){
